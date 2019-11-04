707cb8053dadb5dce883df1ed600271c90ff69c7
tinh <39787214+letinh6498@users.noreply.github.com>
2019-11-04 23:21:35 +0700

    Update danhba-edit.php

--- View/danhba-edit.php
+++ View/danhba-edit.php
@@ -38,13 +38,13 @@ disconnect_db();
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Thêm sinh vien</title>
+        <title>Thêm danh bạ</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
-        <h1>Thêm sinh vien </h1>
+        <h1>Thêm danh bạ </h1>
       
         <div class="container">
         <div class="row">
